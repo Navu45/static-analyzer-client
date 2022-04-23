ba44e6964fba473b65a9ae049c2ee7236ceec90c
Alexey Yamoncheryaev <jamoncheryev1@gmail.com>
2022-04-23 15:18:53 +0300

    [add] authorisation

--- src/components/sites/home.jsx
+++ src/components/sites/home.jsx
@@ -1,6 +1,24 @@
-function Home(){
-    return(
-        <h1>Welcome to Architecture control tool</h1>
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { useIsAuthenticated } from "@azure/msal-react";
+import {SignInButton, SignOutButton} from "../authorisation/sign-buttons";
+
+/**
+ * Renders the navbar component with a sign-in button if a user is not authenticated
+ */
+export function Home() {
+    const isAuthenticated = useIsAuthenticated();
+
+    return (
+        <>
+            <Navbar bg="primary" variant="dark">
+                { isAuthenticated ? <SignOutButton /> : <SignInButton /> }
+            </Navbar>
+            <h5><center>Аутентификация на основе билблиотеки от Microsoft для React</center></h5>
+            <br />
+            <br />
+        </>
     );
-}
+};
+
 export default Home;
--- src/components/sites/profile.jsx
+++ src/components/sites/profile.jsx
@@ -1,6 +1,61 @@
-function Profile(props){
-    return(
-        <p>{props.name}</p>
+import React from "react";
+import {callMsGraph} from "../authorisation/graph";
+import * as PropTypes from "prop-types";
+
+
+const {loginRequest} = require("../authorisation/authConfig");
+const {useMsal} = require("@azure/msal-react");
+const {useState} = require("react");
+const {Button} = require("react-bootstrap");
+
+/**
+ * Renders information about the user obtained from Microsoft Graph
+ */
+export const ProfileData = (props) => {
+    return (
+        <div id="profile-div">
+            <p><strong>First Name: </strong> {props.graphData.givenName}</p>
+            <p><strong>Last Name: </strong> {props.graphData.surname}</p>
+            <p><strong>Email: </strong> {props.graphData.userPrincipalName}</p>
+            <p><strong>Id: </strong> {props.graphData.id}</p>
+        </div>
+    );
+};
+
+
+ProfileData.propTypes = {graphData: PropTypes.any};
+
+function Profile() {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    const name = accounts[0] && accounts[0].name;
+
+    function RequestProfileData() {
+        const request = {
+            ...loginRequest,
+            account: accounts[0]
+        };
+
+        // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+        instance.acquireTokenSilent(request).then((response) => {
+            callMsGraph(response.accessToken).then(response => setGraphData(response));
+        }).catch((e) => {
+            instance.acquireTokenPopup(request).then((response) => {
+                callMsGraph(response.accessToken).then(response => setGraphData(response));
+            });
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {name}</h5>
+            {graphData ?
+                <ProfileData graphData={graphData} />
+                :
+                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
+            }
+        </>
     );
 }
 export default Profile;
